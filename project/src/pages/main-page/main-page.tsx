7b4fa46d4ace065b651be9edb52ae22c46f863d8
Nikolay Berdnikov <n.a.berdnikov@gmail.com>
2022-10-27 10:05:01 +0300

    Tabs go to separate component

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -1,4 +1,5 @@
 import Card from '../../components/card/card';
+import Tabs from '../../components/tabs/tabs';
 
 type Props = {
   offerCount: number;
@@ -8,42 +9,7 @@ function MainPage({ offerCount }: Props): JSX.Element {
   return (
     <main className="page__main page__main--index">
       <h1 className="visually-hidden">Cities</h1>
-      <div className="tabs">
-        <section className="locations container">
-          <ul className="locations__list tabs__list">
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
-                <span>Paris</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
-                <span>Cologne</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
-                <span>Brussels</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item tabs__item--active">
-                <span>Amsterdam</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
-                <span>Hamburg</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
-                <span>Dusseldorf</span>
-              </a>
-            </li>
-          </ul>
-        </section>
-      </div>
+      <Tabs />
       <div className="cities">
         <div className="cities__places-container container">
           <section className="cities__places places">
